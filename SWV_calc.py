69f1fc1d807f1c921befd98f8d3590b5b35ef2b8
Rhianne <rh208@duke.edu>
2020-02-19 23:29:21 -0500

    Added SWV to rigidity calculation with dummy data.

--- SWV_calc.py
+++ SWV_calc.py
@@ -1,15 +1,25 @@
 def main():
-    raw_input = 0
+    raw_input = data_in()
     SWV = raw_to_SWV(raw_input)
     rig = SWV_to_rig(SWV)
     return rig
+
+
+def data_in():
+    # this is where we get input
+    raw_input = 0
+    return raw_input
+
 
 def raw_to_SWV(raw_input):
     SWV = 0
     return SWV
 
+
 def SWV_to_rig(SWV):
-    rig = 0
+    rho = 1000 # kg/m^3
+    rig = 3*rho*(SWV**2)
+    print(rig)
     return rig
 
 
