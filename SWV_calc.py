4b47f8c57dd459d2357dbe4fb582c626187fcfc4
Rhianne <rh208@duke.edu>
2020-02-19 23:23:48 -0500

    Initial commit. Basic framework with input to SWV and SWV to rigidity.

---
+++ SWV_calc.py
@@ -0,0 +1,18 @@
+def main():
+    raw_input = 0
+    SWV = raw_to_SWV(raw_input)
+    rig = SWV_to_rig(SWV)
+    return rig
+
+def raw_to_SWV(raw_input):
+    SWV = 0
+    return SWV
+
+def SWV_to_rig(SWV):
+    rig = 0
+    return rig
+
+
+
+if __name__ == '__main__':
+    main()
