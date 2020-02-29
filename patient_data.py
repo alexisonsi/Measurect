be9791cd686eb25219d83c4291cfc15b449d2ca7
Rhianne <rh208@duke.edu>
2020-02-29 16:10:48 -0500

    Initial commit with functions to retrieve patient IDs and patient files (from databse or local computer).

---
+++ patient_data.py
@@ -0,0 +1,16 @@
+def main():
+    retrieve_ids()
+    patient_files(id)
+
+
+def retrieve_ids():
+    id_list = ['1', '2', '3']
+    return id_list
+
+def patient_files(id):
+    file_list = ['File 1', 'File 2']
+    return file_list
+
+
+if __name__ == '__main__':
+    main()
