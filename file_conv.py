fe0800fdea2fe6874193f7ad5ef4445f96f4da10
Rhianne <rh208@duke.edu>
2020-03-04 12:39:22 -0500

    Troubleshooting csv reader function with test csv.

--- file_conv.py
+++ file_conv.py
@@ -1,12 +1,19 @@
+import json
+import pytest
+import csv
+import os
+cwd = os.getcwd()
+
+
 def import_SWV():
-    # file = open(f'{cwd}/test_data/{csvname}.csv', 'r')
-    # test_data_file = csv.reader(file, delimiter=",")
-    # for row in test_data_file:
-    #     rows.append(row)
-    # file.close()
-    # return rows
-    data = []  # receive from GUI code or local computer
-    return data
+    tSWVpairs = []
+    file = open(f'{cwd}/test_csv.csv', 'r')
+    test_data_file = csv.reader(file, delimiter=",")
+    for row in test_data_file:
+        tSWVpairs.append(row)
+    file.close()
+    return tSWVpairs
+    return tSWVpairs
 
 
 def export_data(data):
@@ -18,23 +25,23 @@ def export_data(data):
 
 
 def to_csv(data):
-    if len(data) == 1:
+    if data == 1:
         print("Convert to csv")
     else:
-        for i in len(data):
+        for i in data:
             print("Convert to csv")
     return
 
 
 def to_LabChart(data):
-    if len(data) == 1:
+    if data == 1:
         print("Export to LabChart")
     else:
-        for i in len(data):
+        for i in data:
             print("Export to LabChart")
     return
 
 
 if __name__ == '__main__':
-    data = import_from_Matlab()
+    data = import_SWV()
     export_data(data)
